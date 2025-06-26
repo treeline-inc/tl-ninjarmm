56acd527a815a1ba2494f176885ba63f80224b91
esf-tlai <eric@treeline.ai>
2025-06-26 16:07:01 -0700

    [TRE-362] Update generated NinjaRMM client (#3)
    ## Project Management
    
    [TRE-362: Update generated NinjaRMM client](https://linear.app/treeline/issue/TRE-362/update-generated-ninjarmm-client)
    
    ## Description
    - Removed the non-`SystemApi` classes and the unnecessary models associated with these classes/endpoints.
       - Initially, the main data we are syncing are the ["core system entities"](https://app.ninjarmm.com/apidocs/?links.active=core#/system) that are used in Ninja.
       - The Ninja OpenAPI spec separates these into different API categories, hence why there are requests that are specific to the `SystemApi` (e.g. we have `GetOrganizationsDefaultResponseInner` as a model that corresponds to getting a list of organizations, but not `GetOrganizationDefaultResponseInner` that would correspond to getting a single organization by id). 
    
    ## Testing
    Manually tested the following `SystemApi` queries:
    - [x] Organizations (Detailed) `SystemApi(client).get_organizations_detailed_with_http_info()`
    - [x] Devices (Detailed) `SystemApi(client).get_devices_detailed_with_http_info()`
    - [x] Users: `SystemApi(client).get_users_with_http_info()`
    - [x] Policies: `SystemApi(client).get_policies_with_http_info()`
    - [x] Roles: `SystemApi(client).get_node_roles_with_http_info()`

--- src/tl_ninjarmm/api/__init__.py
+++ src/tl_ninjarmm/api/__init__.py
@@ -1,23 +1,4 @@
 # flake8: noqa
 
 # import apis into api package
-from tl_ninjarmm.api.asset_tags_api import AssetTagsApi
-from tl_ninjarmm.api.backup_api import BackupApi
-from tl_ninjarmm.api.checklist_templates_api import ChecklistTemplatesApi
-from tl_ninjarmm.api.custom_fields_api import CustomFieldsApi
-from tl_ninjarmm.api.document_templates_api import DocumentTemplatesApi
-from tl_ninjarmm.api.knowledge_base_articles_api import KnowledgeBaseArticlesApi
-from tl_ninjarmm.api.location_api import LocationApi
-from tl_ninjarmm.api.organization_checklists_api import OrganizationChecklistsApi
-from tl_ninjarmm.api.organization_documents_api import OrganizationDocumentsApi
-from tl_ninjarmm.api.related_items_api import RelatedItemsApi
-from tl_ninjarmm.api.vulnerability_management_api import VulnerabilityManagementApi
-from tl_ninjarmm.api.devices_api import DevicesApi
-from tl_ninjarmm.api.groups_api import GroupsApi
-from tl_ninjarmm.api.management_api import ManagementApi
-from tl_ninjarmm.api.organization_api import OrganizationApi
-from tl_ninjarmm.api.queries_api import QueriesApi
 from tl_ninjarmm.api.system_api import SystemApi
-from tl_ninjarmm.api.ticketing_api import TicketingApi
-from tl_ninjarmm.api.webhooks_api import WebhooksApi
-
