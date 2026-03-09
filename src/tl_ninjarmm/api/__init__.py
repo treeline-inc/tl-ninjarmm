d45d673d3bef1725dab844ad7a8aa5a46443967c
Nate Armstrong <nate@treeline.ai>
2026-03-09 23:31:56 +0000

    [TRE-1619] Add more endpoints to SDK
    Add the management and devices APIs to the SDK.

--- src/tl_ninjarmm/api/__init__.py
+++ src/tl_ninjarmm/api/__init__.py
@@ -1,4 +1,7 @@
 # flake8: noqa
 
 # import apis into api package
+from tl_ninjarmm.api.devices_api import DevicesApi
+from tl_ninjarmm.api.management_api import ManagementApi
 from tl_ninjarmm.api.system_api import SystemApi
+
